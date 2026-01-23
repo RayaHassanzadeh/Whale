a4da2921bc7df4bbafbdfdc9b944d24037853523
RayaHassanzadeh <2776973@learn.vsb.bc.ca>
2026-01-23 12:39:32 -0800

    Update scripts.js

--- statics/scripts.js
+++ statics/scripts.js
@@ -1,9 +1,9 @@
-// 自动隐藏 Flash 消息
+// automatically hide flasg messages
 setTimeout(() => {
     const flashMessages = document.querySelectorAll('.alert');
     flashMessages.forEach((message) => {
-        message.classList.remove('show');  // 移除 Bootstrap 的 "show" 类
-        message.classList.add('fade');    // 添加 Bootstrap 的 "fade" 类
-        setTimeout(() => message.remove(), 500);  // 确保动画完成后移除消息
+        message.classList.remove('show');  
+        message.classList.add('fade');    
+        setTimeout(() => message.remove(), 500);  
     });
-}, 3000); // 设置 3 秒后自动隐藏
+}, 3000); // sets the screen to automatically hide after 3 seconds
