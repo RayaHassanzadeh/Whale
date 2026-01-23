f1cdf28fcc44f0a89b1687b46859f37b7f0aee20
RayaHassanzadeh <2776973@learn.vsb.bc.ca>
2026-01-23 12:53:27 -0800

    Update run.py

--- RUN/run.py
+++ RUN/run.py
@@ -7,4 +7,4 @@ from forum import db
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5008, debug=True)
+    app.run(host='0.0.0.0', port=5009, debug=True)
